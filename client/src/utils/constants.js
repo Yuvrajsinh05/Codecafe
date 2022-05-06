3e5666ddfd0f126d2623dd6ab9f219769038643a
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-05-06 14:52:31 +0530

    06/05/2022

---
+++ client/src/utils/constants.js
@@ -0,0 +1,8 @@
+
+import abi from './Transactions.json';
+
+
+export const contractAddress = '0x259d597ca7E2f957D89e3EAE3359Ce2eEd7bE085';
+export const contractABI = abi.abi;
+
+
---
+++ smart_contracts/hardhat.config.js
@@ -0,0 +1,10 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports={
+    solidity: '0.8.0',
+    networks:{
+      ropsten:{
+        url:'https://eth-ropsten.alchemyapi.io/v2/uv4isBIkoQLqRXtrixTijJ_mVF5mbf7K',
+        accounts:['d1c4c17700243c6e1c1350ffe9bde60c9e7a430b9edc9aea827c549123309a14']
+    }
+}}
